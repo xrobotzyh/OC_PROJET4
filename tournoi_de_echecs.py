1b281d423298afab3c99417f6710cd2e7aa26848
zhao yh <makai87kai@outlook.com>
2023-03-16 21:53:20 +0100

    add 2 functions for generate the list of opponent,generate_random_opponent seems to be ok, generate_opponent need to finish,add some explications to the class and functions

--- tournoi_de_echecs.py
+++ tournoi_de_echecs.py
@@ -1,11 +1,18 @@
-class Players:
+import random
 
+
+class Players:
+    '''
+        a class that contains all the players informations and can calculate the scores after each round
+        of the games
+    '''
     def __init__(self,first_name,last_name,birthday,scores = 0):
         self.first_name =  first_name
         self.last_name = last_name
         self.birthday = birthday
         self.scores = scores
     def score(self,winning_situation_type):
+    # winner + 1 point, loser 0 point, otherwise 0.5 point for each
         if winning_situation_type == 1:
             self.scores = self.scores + 1
         elif winning_situation_type == 0:
@@ -14,6 +21,7 @@ class Players:
             self.scores = self.scores + 0.5
         return print(self.scores)
     def print_informations_players(self):
+    # a dictionary with informations of players
         informations_player = {
                                "First Name":self.first_name,
                                "Last Name":self.last_name,
@@ -21,9 +29,13 @@ class Players:
                                "Scores":self.scores
                                }
         print(informations_player)
+    def list_of_players(self):
 
 
 class Championships:
+    '''
+        a class that contains all the necessary informations of the championships
+    '''
     round_indicator = 1
     def __init__(self,name,place,starting_date,ending_date,description,
                  player,round_indicator=1,round_total=4):
@@ -36,6 +48,7 @@ class Championships:
         self.player = player
         self.round_indicator = round_indicator
     def round(self):
+    # a round indicator
         self.round_indicator = self.round_indicator + 1
         return round_indicator
     def print_informations_championship(self):
@@ -55,9 +68,39 @@ class Match(Championships):
         super().__init__(self, round_indicator)
         self.round = round_indicator
 
-def generateopponent(listplayer):
-    pass
+def generate_random_opponent(List_Player):  -> Tuple[Dict[int,str]]
+    '''
+        for the first list of opponent,we use random function
+    '''
+    List_Player_Opponent = []   # a new list that contains list of opponent
+    List_Temporary = []   # a temporary list
+    List_Player_Opponent_round = {}
+    i = 0
+    random.shuffle(List_Player)
+    while i < len(List_Player):
+        List_Temporary = List_Player[i:i+2]
+        i = i + 2
+        List_Player_Opponent.append(List_Temporary)
+    List_Player_Opponent_round = {1:List_Player_Opponent}
+    return List_Player_Opponent_round
 
+def generate_opponent(List_Player,List_Player_Opponent_round[]):
+    '''
+        generate list of opponent by the order of there points
+    '''
+    i = 0
+    New_List_Player_Opponent = []  # a new list that contains list of opponent
+    List_Temporary = []  # a temporary list
+    while i < len(List_Player):
+        List_Temporary.append(List_Player[0])
+        List_Temporary.append(List_Player[i+1])
+        #if List_Temporary in List_Player_Opponent :
+        #   i = i + 1
 
-listplayer = []
+        #ListPlayerOpponent.append(List_Temporary)
+    return List_Player_Opponent_round
+
+ListPlayer = []
+List_Player_Opponent
+
 listmatch = []
