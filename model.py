64e83ff2c89fdb30cd325a88d0d561489e3188fd
Antoine Latrille <antoine.latrille@gmail.com>
2023-03-31 16:15:09 +0200

    Retours sur la semaine de travail: - Reprise de la navigation - Reprise de la vue et des responsabilites - Implementation du create user et show user - Re-structutation des fonctions de load from json - Revue du code pour la generation des pairs de joueurs

--- model.py
+++ model.py
@@ -1,8 +1,18 @@
-from typing import List
+from random import random
+from typing import List, Dict, Any
 from datetime import datetime
 
+
 class Player():
-    def __init__(self, id:int, first_name:str, last_name:str, birthday:datetime, club_id:str= "AB12345"):
+    # dictionnary of field_name: field description to create a new Player
+    INPUT_FIELDS = {
+        'first_name': "Player's first name",
+        'last_name': "Player's last name",
+        'birthday': "Player's birthday DD/MM/YYYY",
+        'club_id': "Player's club id"
+    }
+
+    def __init__(self, id: int, first_name: str, last_name: str, birthday: datetime, club_id: str = "AB12345"):
         self.score = 0
         self.id = id or 1
         self.first_name = first_name
@@ -10,41 +20,57 @@ class Player():
         self.birthday = birthday
         self.club_id = club_id
 
-    def get_player(self):
+    @classmethod
+    def from_values(cls, values: Dict[str, Any]):
+        birthday = datetime.strptime(values['birthday'], "%d/%m/%Y")
+        user_id = int(values['id'])
+        return cls(
+            id=user_id,
+            first_name=values['first_name'],
+            last_name=values['last_name'],
+            birthday=birthday,
+            club_id=values['club_id']
+        )
+
+    def as_dict(self):
         player = {
-            "Player's id" : self.id,
-        "Player's first name" : self.first_name,
-        "Player's last name" : self.last_name,
-        "Player's birthday DD/MM/YYYY" : self.birthday,
-        "Player's club id" : self.club_id
+            "id": self.id,
+            "first name": self.first_name,
+            "last name": self.last_name,
+            "birthday DD/MM/YYYY": self.birthday,
+            "club id": self.club_id
         }
         return player
 
     def get_player_with_score(self):
         player_with_score = {
-            "player :":self.get_player(),
-            "score :":self.score
+            "player :": self.as_dict(),
+            "score :": self.score
         }
         return player_with_score
 
+
 class Match:
-    def __init__(self,player_a:Player,player_b:Player):
+    def __init__(self, player_a: Player, player_b: Player):
         self.player_a = player_a
         self.player_b = player_b
         self.result = None
-    def get_match(self,round):
+
+    def get_match(self, round):
         self.round = round
         match = {
-            "round :":self.round,
-            "Player A :":self.player_a,
-            "Player B :":self.player_b,
-            "Result :":self.result
+            "round :": self.round,
+            "Player A :": self.player_a,
+            "Player B :": self.player_b,
+            "Result :": self.result
         }
         return match
 
-class Tournament():
-    def __init__(self, name:str, location:str, start_date:datetime, end_date:datetime, number_round:int,
-                 description:str, total_number_round:int, player_id:list=None):
+
+class Tournament:
+    def __init__(self, id: int, name: str, location: str, start_date: datetime, end_date: datetime, number_round: int,
+                 description: str, total_number_round: int, player_id: list = None):
+        self.id = id
         self.name = name
         self.location = location
         self.start_date = start_date
@@ -52,8 +78,13 @@ class Tournament():
         self.number_round = number_round
         self.description = description
         self.total_number_round = total_number_round
-        self.players = player_id
-        
+        self.players = player_id  # WARNING fixme
+
+        self.current_round = 1
+        self.current_round_matches = []
+        self.rounds = []
+        self.players_scores = {}
+
     def get_tournament(self):
         tournament = {
             "Name": self.name,
@@ -70,3 +101,28 @@ class Tournament():
     def is_finished(self):
         now = datetime.now()
         return self.end_date <= now
+
+    def start(self):
+        self.current_round_matches = self.generate_first_round_match_pairs()
+        self.players_scores = ...
+
+    def go_to_next_round(self):
+        self.rounds.append(self.current_round_matches)
+        self.current_round += 1
+        self.current_round_matches = self.generate_round_match_pairs()
+
+    def generate_first_round_match_pairs(self):
+        # FIXME utiliser les scores des joueurs pour bien faire
+        random.shuffle(self.players)
+        if (len(self.players)) % 2 == 0:
+            list_temporary = [self.players[i:i + 2] for i in range(0, len(self.players), 2)]
+            list_player_opponent_round = {1: list_temporary}
+            return list_player_opponent_round
+        else:
+            list_temporary = [self.players[i:i + 2] for i in range(0, len(self.players) - 1, 2)]
+            list_temporary.append([self.players[-1], None])
+            list_player_opponent_round = {"1": list_temporary}
+            return list_player_opponent_round
+
+    def generate_round_match_pairs(self):
+        pass
--- view.py
+++ view.py
@@ -1,147 +1,91 @@
-from model import Player
+from typing import Dict
 
 
 class View():
 
-    def __init__(self,header, footer):
+    def __init__(self, header, footer):
         self.header = header
         self.header_main = "*What do you want to do ?\n"
         self.header_add_tournament = "*Please complete the information below"
-        self.footer = "please type the number for selecting \n"
+        self.footer = "\nPlease type the number for selecting \n"
 
-    def display_menu(self):
-
-        # a block to show add a new tournament,after the input return to the main menu
-
-        print(self.header_main)
-        self.menu = {
-            0: "Manage Players",
-            1: "Manage tournament",
-            2: "Show Reports",
-            3: "Load/Sava data",
-            4: "Exit"
-        }
-        self.display_dicts(self.menu)
-        choice = input(self.footer)
-        return choice
-
-    def display_manage_players_menu(self):
-        self.menu = {
-            0: "Add a new player",
-            1: "Show all the players",
-            2: "Updating one player",
-            3: "Back"
-        }
-        self.display_dicts(self.menu)
-        choice = input(self.footer)
-        return choice
-
-    def display_tournament_manage_menu(self):
-        self.menu = {
-            0: "Add a new tournament",
-            1: "update a tournament",
-            2: "Manage the current tournament",
-            3: "Show past tournaments",
-            4: "Back"
-        }
-        self.display_dicts(self.menu)
-        choice = input(self.footer)
-        return choice
-
-    def display_exit(self):
-        print("See you next time !")
-        exit()
-    def display_show_rapports_memu(self):
-        pass
-
-    def display_data_menu(self):
-        pass
-
-    def display_add_new_player(self):
-        answer = "Y"
-        new_player = []
-        while answer == "Y":
-            new_player.append(input(self.update_dicts(Player(None, None, None, None, None).get_player())))
-            answer = input("Do you want to add a player? Y or N\n")
-        return new_player   #list
-
-    def display_add_new_tournament_menu(self):
+    def display_menu(self, choices: Dict[str, str]):
 
         # a block to show add a new tournament,after the input return to the main menu
-
-        tournament = Tournament().get_tournament()  # initialization data
-        tournament.pop("List of players participate")  # delete the element "player" handle it separately
         print(self.header_main)
-        print(self.header_add_tournament)
-        tournament = update_dicts(tournament)
-        player = []
-        reponse = input("Do you want to add player? Y/N \n")
-        while reponse in ["y", "Y"]:
-            player = create_a_new_player(player)
-            reponse = input("Do you want to add player? Y/N \n")
-        tournament.update({"List of players participate": player})
-        print(display_dicts(tournament))
-        return tournament
-
-    def display_current_tournament(self, tournaments):
-        print(self.header_main)
-        tournament = Tournament()
-        for index, tournament in enumerate(tournaments):
-            print(f'{index + 1} : {tournament["Name"]}')
-        choice = input("\n*Select the tournament you want to manage by typing its number \n")
+        choice = None
+        self._display_dicts(choices)
+        while choice not in choices:
+            choice = input(self.footer)
         return choice
 
-    def display_manage_tournament(self):
-        print(self.header_main)
-        dict_manage_tournament = {
-            0: "Add a new player to the tournament",
-            1: "Generate next match",
-            2: "Add the result of last round",
-            3: "Show the history results"
-        }
-        display_dicts(dict_manage_tournament)
-        choice = input(self.footer)
-        return choice
+    def get_user_inputs(self, input_fields: Dict[str, str]) -> Dict[str, str]:
+        # Note for improvements: user input validation
+        user_inputs = {}
+        for key, value in input_fields.items():
+            value = input(f'Input for {value}\n')
+            user_inputs[key] = value
+        return user_inputs
 
-    def display_previous_tournament(self, tournaments):
-        print(self.header_main)
-        tournament = Tournament()
-        for index, tournament in enumerate(tournaments):
-            print(f'{index + 1} : {tournament["Name"]}')
-        choice = input("\n*Select the tournament you want to manage by typing its number \n")
-        return choice
+    def display_message(self, message):
+        print(message)
 
-    def display_no_tournament_at_this_moment(self):
-        if not os.path.exists('resources') or len(new_tournaments) == 0:
-            print('**//!!!There is no tournament for the moment!!!//**\n\n\n')
-
-
-    def display_dicts(self,dicts):
+    # def _display_lists(self, lists):
+    #     for i in range(len(lists)):
+    #         print(i)
+    def _display_dicts(self, dicts):
         # a function that will show the dictionary line by line
 
         for key, value in dicts.items():
-            print(f'{key}:{value}')
+            print(f'{key}: {value}')
 
-    def display_lists(self,lists):
-        for i in range(len(lists)):
-            print(i)
+    # def display_add_new_tournament_menu(self):
+    #
+    #     # a block to show add a new tournament,after the input return to the main menu
+    #
+    #     tournament = Tournament().get_tournament()  # initialization data
+    #     tournament.pop("List of players participate")  # delete the element "player" handle it separately
+    #     print(self.header_main)
+    #     print(self.header_add_tournament)
+    #     tournament = update_dicts(tournament)
+    #     player = []
+    #     reponse = input("Do you want to add player? Y/N \n")
+    #     while reponse in ["y", "Y"]:
+    #         player = create_a_new_player(player)
+    #         reponse = input("Do you want to add player? Y/N \n")
+    #     tournament.update({"List of players participate": player})
+    #     print(display_dicts(tournament))
+    #     return tournament
+    #
+    # def display_current_tournament(self, tournaments):
+    #     print(self.header_main)
+    #     tournament = Tournament()
+    #     for index, tournament in enumerate(tournaments):
+    #         print(f'{index + 1} : {tournament["Name"]}')
+    #     choice = input("\n*Select the tournament you want to manage by typing its number \n")
+    #     return choice
+    #
+    # def display_manage_tournament(self):
+    #     print(self.header_main)
+    #     dict_manage_tournament = {
+    #         0: "Add a new player to the tournament",
+    #         1: "Generate next match",
+    #         2: "Add the result of last round",
+    #         3: "Show the history results"
+    #     }
+    #     display_dicts(dict_manage_tournament)
+    #     choice = input(self.footer)
+    #     return choice
 
+    # def display_previous_tournament(self, tournaments):
+    #     print(self.header_main)
+    #     tournament = Tournament()
+    #     for index, tournament in enumerate(tournaments):
+    #         print(f'{index + 1} : {tournament["Name"]}')
+    #     choice = input("\n*Select the tournament you want to manage by typing its number \n")
+    #     return choice
 
-    def update_dicts(self,dict):
-        # a function that will update the data after input manipulations by users
-        new_dict = {}
-        for key, value in dict.items():
-            value = input(f'Please enter the {key}\n')
-            key = key
-            new_dict.update({key: value})
-        return new_dict
-
-
-
-    def update_one_player(self,playerid:str,listplayers:list):
-        for player in listplayers :
-            if player["Player's id"] == playerid:
-                update_dicts(player)
-            else :
-                print(f"There is no such player with id {playerid}")
-
+    # def display_no_tournament_at_this_moment(self):
+    #     if not os.path.exists('resources') or len(new_tournaments) == 0:
+    #         print('**//!!!There is no tournament for the moment!!!//**\n\n\n')
+    #
